6c778420c6438edac48ae12d0d253ea591be3686
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 15:17:28 +0600

    create button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React, {createElement as e} from 'react';
 
 function App() {
-//  return  (
-//    <h1>Hello react</h1>
-//  )
   return e('div', {className: 'container'}, [
     e('h1', {className: 'font-bold', key: 1}, 'Test JSX'),
-    e('button', {className: '', key: 2}, 'Click me!')
+    e('button', {className: 'py-2 px-4 border', key: 2}, 'Click me!')
   ])
 }
 
