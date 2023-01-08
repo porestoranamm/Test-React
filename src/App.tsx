c1fd003687e601edd0ba1fcae6be7288c8189530
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 15:15:44 +0600

    create import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {createElement as e} from 'react';
 
 function App() {
-  return  (
-    <h1>Hello react</h1>
-  )
+//  return  (
+//    <h1>Hello react</h1>
+//  )
+  return e('div', {className: 'container'}, [
+    e('h1', {className: 'font-bold', key: 1}, 'Test JSX'),
+    e('button', {className: '', key: 2}, 'Click me!')
+  ])
 }
 
 export default App;
