8f0f1a54193b4ebf4b1222e54ec2f57074a17c82
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 16:16:01 +0600

    create code components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React, {createElement as e, useState} from 'react';
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
 
-  return e('div', {className: 'container'}, [
-    e('h1', {className: 'font-bold', key: 1}, `Test JSX ${count}`),
-    e('button', {
-      className: 'py-2 px-4 border', 
-      key: 2, 
-      onClick: () => setCount(count + 1)
-    }, 'Click me!')
-  ])
+    </div>
+  )
 }
 
 export default App;
