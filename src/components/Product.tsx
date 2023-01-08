3b6d685b3be49a947877d6dee8a053acdbef88c4
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 17:08:34 +0600

    import product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {IProduct} from '../models' 
 
-export function Product() {
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: any) {
     return (
         <div
             className="border py-2 px-4 rounded flex-col items-center mb-2" 
-        >Product!</div>
+        >
+            Product!
+        </div>
     )
 }
