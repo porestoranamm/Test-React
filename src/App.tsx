e6db1e97a243f28bb747f2d094e8e99380a2edad
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 15:19:48 +0600

    add onClick

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import React, {createElement as e} from 'react';
 function App() {
   return e('div', {className: 'container'}, [
     e('h1', {className: 'font-bold', key: 1}, 'Test JSX'),
-    e('button', {className: 'py-2 px-4 border', key: 2}, 'Click me!')
+    e('button', {
+      className: 'py-2 px-4 border', 
+      key: 2, 
+      onClick: () => console.log('Clicked!')
+    }, 'Click me!')
   ])
 }
 
