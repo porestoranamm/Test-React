d9fdf7d1aefe4203e59b836abeb5c62712e7fda0
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-08 16:22:20 +0600

    add product in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {Product} from './components/Product'
 
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
+      <Product />
 
     </div>
   )
