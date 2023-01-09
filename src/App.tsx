356dbbad60a04ebe021a0fbed6473b5045649143
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-09 22:59:47 +0600

    add error message

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,21 @@ import { IProduct } from './models'
 function App() {
   const [products, setProducts] = useState<IProduct[]>([])
   const [loading, setLoading] = useState(false)
+  const [error,setError] = useState('')
 
   async function fetchProducts() {
+    try {
+    setError('')
     setLoading(true)
     const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
     setProducts(response.data)
     setLoading(false)
+    } catch (e: unknown) {
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+    
   }
 
   useEffect(() => {
@@ -22,6 +31,7 @@ function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
       { products.map(product => <Product product={product} key={product.id} />) }
       
     </div>
