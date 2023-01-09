bb4bf46295d34afd800f8db4de7475e7ec53bd6e
Nikita Kuznetsov <livisback@yandex.ru>
2023-01-09 22:49:54 +0600

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import {Product} from './components/Product'
 import { IProduct } from './models'
 
 function App() {
-  
   const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
 
   async function fetchProducts() {
+    setLoading(true)
     const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
     setProducts(response.data)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -19,7 +21,7 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      
+      {loading && <p className="text-center">Loading...</p>}
       { products.map(product => <Product product={product} key={product.id} />) }
       
     </div>
